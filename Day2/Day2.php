584987306d5fe336a02ffde0e467c4b12d7b08d1
James Alexander <james@jamesryanalexander.com>
2024-12-03 02:34:40 -0500

    silly solution but it works. page only does part 2 answer now, for part 1 answer see earlier version

--- Day2/Day2.php
+++ Day2/Day2.php
@@ -10,72 +10,100 @@ $howmanyreports = 0;
 // iterate over each line as it's own report
 foreach ($day2input as $i => $report) {
 	//add line to it's own array to iterate off
-	$report_array = explode(" ", $report);
+	$original_report_array = explode(" ", $report);
 	//find last key for this report so that we know when to end and consider "safe"
-	$lastkey = array_key_last($report_array);
+	$lastkey_original = array_key_last($original_report_array);
 	//increment the report tracker for testing and echo
 	$howmanyreports++;
-	//keeping track of last report number as we go through the rules, each line should start at 0
-	$lastreportnum = 0;
-	// need to track if the line is going up or down
-	$goingup = null;
-	$goingdown = null;
-	//iterate over rules for each report line
-	foreach ($report_array as $ir => $reportnum) {
-		//start off checking if this is the first report (nothing to compare to)
-		if ($lastreportnum == 0) {
-			//if so set to lastreport num and report out to terminal
-			$lastreportnum = $reportnum;
-			// otherwise check if it's less 
-			continue;
-		} elseif (($lastreportnum > $reportnum)) {
-			if (($goingdown !== false)) {
-				$goingdown = true;
-				$goingup = false;
-					if ($lastreportnum - $reportnum <= 3) {
-						// difference less than 3 so not going down too fast
-							if ($ir == $lastkey) {
-								$safereports++;
-								break;
-							}
-						$lastreportnum = $reportnum;
-						continue;
-					} else {
-						break;
-					}
-			} else {
-				break;
-			}
-			
 
-		} elseif (($lastreportnum < $reportnum)) {
-			if (($goingup !== false)) {
-				$goingdown = false;
-				$goingup = true;
-					if ($reportnum - $lastreportnum <= 3) {
-						// difference less than 3 so not going down too fast
-							if ($ir == $lastkey) {
-								$safereports++;
+
+//making an array of arrays to test them all for part 2 (to see if they'd work with 1 error) this is silly, there has to be a better way but not today lol
+		$numberofreports = 1 + $lastkey_original;
+		$valuetodelete = 0;
+		$array_of_report_arrays = array();
+		$array_of_report_arrays[] = $original_report_array;
+		while ($numberofreports > 0) {
+			$new_report_array = $original_report_array;
+			array_splice($new_report_array, $valuetodelete, 1);
+			$array_of_report_arrays[] = $new_report_array;
+			$numberofreports--;
+			$valuetodelete++;
+		}
+
+	// now iterate over the silly array of report arrays
+		foreach ($array_of_report_arrays as $ira => $report_array) {
+			$lastkey = array_key_last($report_array);
+			//keeping track of last report number as we go through the rules, each line should start at 0
+			$lastreportnum = 0;
+			// need to track if the line is going up or down
+			$goingup = null;
+			$goingdown = null;
+
+			//iterate over rules for each report line
+			foreach ($report_array as $ir => $reportnum) {
+
+				//start off checking if this is the first report (nothing to compare to)
+				if ($lastreportnum == 0) {
+					//if so set to lastreport num and report out to terminal
+					$lastreportnum = $reportnum;
+					// otherwise check if it's less 
+					continue;
+				} elseif (($lastreportnum > $reportnum)) {
+					// is goingdown already set as false (because of earlier iteration) if so changed direction, if not continue
+					if (($goingdown !== false)) {
+						$goingdown = true;
+						$goingup = false;
+							// check difference
+							if ($lastreportnum - $reportnum <= 3) {
+								// difference less than 3 so not going down too fast
+								// check if this is the last key (and so known safe now)
+									if ($ir == $lastkey) {
+										$safereports++;
+										break 2;
+									}
+								$lastreportnum = $reportnum;
+								continue;
+							} else {
 								break;
 							}
-						$lastreportnum = $reportnum;
-						continue;
+					} else {
+						break;
+					}	
+
+				} elseif (($lastreportnum < $reportnum)) {
+					// is goingup already set as false (because of earlier iteration) if so changed direction, if not continue
+					if (($goingup !== false)) {
+						$goingdown = false;
+						$goingup = true;
+						// check difference
+							if ($reportnum - $lastreportnum <= 3) {
+								// difference less than 3 so not going down too fast
+									if ($ir == $lastkey) {
+										$safereports++;
+										break 2;
+									}
+								$lastreportnum = $reportnum;
+								continue;
+							} else {
+								break;
+							}
 					} else {
 						break;
 					}
-			} else {
-				break;
+					
+				} elseif (($lastreportnum !== 0) && ($lastreportnum == $reportnum)) {
+					break;
+				} else {
+					echo "huh how did this get here? Check this ".PHP_EOL;
+				}
 			}
-			
-		} elseif (($lastreportnum != 0) && ($lastreportnum == $reportnum)) {
-			break;
-		} else {
+
+
 		}
-	}
 
 	
 }
 
-echo "last report processed! Total number of safe reports: ".$safereports.PHP_EOL
+echo "last report processed! Total number of safe reports: ".$safereports.PHP_EOL;
 
 ?>
